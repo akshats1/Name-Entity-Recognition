0bbf5e8c7c672d91724a063d0935ce94c2a3ad93
akshats1 <akshats063@gmail.com>
2024-04-02 21:23:47 +0530

    requirement file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="Akshat",
+    author_email="akshats063@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
